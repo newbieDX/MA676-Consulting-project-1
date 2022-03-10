999c99f2bb9274258fbbd9cfd9f6c96b6e5f35c0
newbieDX <chenxubu@bu.edu>
2022-03-10 11:31:57 -0500

    update new vfunctions for further analysis

---
+++ new_vfunctions.R
@@ -0,0 +1,128 @@
+source("clean_and_wrangle_data.R")
+library("ggplot2")
+
+# Do GCs who answered YES to Q7 tend to agree with the embryo transfer justification of “quality of life” across conditions (Q24, Q31, Q32, Q108, Q113, Q51, Q110) more often than GCs who answered NO to Q7? 
+
+q7_qol_visual <- q7_qol %>% select(c(1,4:11))
+q7_qol_table <- q7_qol_visual %>%
+  pivot_longer(-c(IPAddress,Q7), names_to = "question_num", values_to = "Response")
+
+q7_qol_visual_f <- function(qnum){
+  df <- q7_qol_table %>% filter(question_num == qnum)
+  
+  ggplot(df, aes(x = Response, fill = Q7))+
+    geom_bar(stat = "count", position = "stack")+
+    facet_grid(~Q7)
+}
+
+# q7_qol_visual_f("Q108")
+
+q7_qol_general <- ggplot(q7_qol_table, aes(x = Response, fill = question_num))+
+  geom_bar(stat = "count", position = "dodge")+
+  facet_grid(~Q7)
+
+
+# Do GCs who answered YES to Q7 tend to agree with the embryo transfer justification of “family history” across conditions (Q26, Q30, Q37, Q40, Q46, Q52, Q58) more often than Do GCs who answered NO to Q7 ?
+
+q7_fh_visual <- q7_fh %>% select(c(1,4:11))
+q7_fh_table <- q7_fh_visual %>%
+  pivot_longer(-c(IPAddress,Q7), names_to = "question_num", values_to = "Response")
+
+q7_fh_visual_f <- function(qnum){
+  df <- q7_fh_table %>% filter(question_num == qnum)
+  
+  ggplot(df, aes(x = Response, fill = Q7))+
+    geom_bar(stat = "count", position = "stack")+
+    facet_grid(~Q7)
+}
+
+q7_fh_general <- ggplot(q7_fh_table, aes(x = Response, fill = question_num))+
+  geom_bar(stat = "count", position = "dodge")+
+  facet_grid(~Q7)
+
+
+# q7_fh_visual_f("Q37")
+  
+# Do GCs who answered YES to Q7 tend to agree with the embryo transfer justification of “Variability” across conditions (Q23, Q27, Q33, Q38, Q45, Q56) more often than GCs who answered NO to Q7?
+
+q7_var_visual <- q7_var %>% select(c(1,4:10))
+q7_var_table <- q7_var_visual %>%
+  pivot_longer(-c(IPAddress,Q7), names_to = "question_num", values_to = "Response")
+
+q7_var_visual_f <- function(qnum){
+  df <- q7_var_table %>% filter(question_num == qnum)
+  
+  ggplot(df, aes(x = Response, fill = Q7))+
+    geom_bar(stat = "count", position = "stack")+
+    facet_grid(~Q7)
+}
+
+# q7_var_visual_f("Q23")
+
+q7_var_general <- ggplot(q7_var_table, aes(x = Response, fill = question_num))+
+  geom_bar(stat = "count", position = "dodge")+
+  facet_grid(~Q7)
+
+  
+# Do GCs who answered YES to Q7  tend to agree with the embryo transfer justification of “reproductive autonomy” across conditions (Q29, Q35, Q36, Q41, Q47, Q53, Q59) more often than GCs who answered NO to Q7 ?
+
+q7_repauto_visual <- q7_repauto %>% select(c(1,4:11))
+q7_repauto_table <- q7_repauto_visual %>%
+  pivot_longer(-c(IPAddress,Q7), names_to = "question_num", values_to = "Response")
+
+q7_repauto_visual_f <- function(qnum){
+  df <- q7_repauto_table %>% filter(question_num == qnum)
+  
+  ggplot(df, aes(x = Response, fill = Q7))+
+    geom_bar(stat = "count", position = "stack")+
+    facet_grid(~Q7)
+}
+
+
+q7_repauto_general <- ggplot(q7_repauto_table, aes(x = Response, fill = question_num))+
+  geom_bar(stat = "count", position = "dodge")+
+  facet_grid(~Q7)
+
+# q7_repauto_visual_f("Q47")
+  
+# Do GCs who answered YES to Q7  tend to agree with the embryo transfer justification of “Resources available” across conditions (Q25, Q107, Q42, Q109, Q54) more often than GCs who answered NO to Q7?
+
+q7_resava_visual <- q7_resava %>% select(c(1,4:9))
+q7_resava_table <- q7_resava_visual %>%
+  pivot_longer(-c(IPAddress,Q7), names_to = "question_num", values_to = "Response")
+
+q7_resava_visual_f <- function(qnum){
+  df <- q7_resava_table %>% filter(question_num == qnum)
+  
+  ggplot(df, aes(x = Response, fill = Q7))+
+    geom_bar(stat = "count", position = "stack")+
+    facet_grid(~Q7)
+}
+
+
+q7_resava_general <- ggplot(q7_resava_table, aes(x = Response, fill = question_num))+
+  geom_bar(stat = "count", position = "dodge")+
+  facet_grid(~Q7)
+
+
+# q7_resava_visual_f("Q54")
+  
+# Do GCs who answered YES to Q7 tend to feel more morally uneasy with transferring affected embryos (Q102_1 - 102_11) more often than GCs who answered NO to Q7
+
+q7_q102_visual <- q7_q102 %>% select(c(1,4:11))
+q7_q102_table <- q7_q102_visual %>%
+  pivot_longer(-c(IPAddress,Q7), names_to = "question_num", values_to = "Response")
+
+q7_q102_visual_f <- function(qnum){
+  df <- q7_q102_table %>% filter(question_num == qnum)
+  
+  ggplot(df, aes(x = Response, fill = Q7))+
+    geom_bar(stat = "count", position = "stack")+
+    facet_grid(~Q7)
+}
+
+# q7_q102_visual_f("Q102_1")
+
+q7_q102_general <- ggplot(q7_q102_table, aes(x = Response, fill = question_num))+
+  geom_bar(stat = "count", position = "dodge")+
+  facet_grid(~Q7)
--- vfunctions.R
+++ vfunctions.R
@@ -15,6 +15,7 @@ q10_q103_visual_f <- function(qnum){
     facet_grid(~Q10)
 }
 
+
 # Relatively high corr qnum: q10 with q103_2, q103_3, q103_4
 
 
@@ -46,6 +47,10 @@ q7_q103_visual_f <- function(qnum){
     geom_bar(stat = "count", position = "stack")+
     facet_grid(~Q7)
 }
+
+# ggplot(q7_q103_table, aes(x = Response, fill = question_num))+
+#   geom_bar(stat = "count", position = "stack")+
+#   facet_grid(~Q7)
 
 # Relatively high corr qnum: q7 with q103_6
 
@@ -80,10 +85,13 @@ q3_q103_visual_f <- function(qnum){
     facet_grid(~Q3)
 }
 
+# ggplot(q3_q103_table, aes(x = Response, fill = question_num))+
+#   geom_bar(stat = "count", position = "stack")+
+#   facet_grid(~Q3)
+
 
 # Relatively high negative corr qnum: q3 with q103_3 
 # Relatively high positive corr qnum: q3 with q103_5
-
 # q3_q103_visual_f("Q103_3")
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “quality of life” across conditions (Q24, Q31, 32, 108, 113, 51, 110) more often than GCs with less years of patient-facing genetic counseling experience? 
@@ -101,7 +109,12 @@ q3_qol_visual_f <- function(qnum){
     facet_grid(~Q3)
 }
 
-# q3_qol_visual_f("Q113")
+
+# ggplot(q3_qol_table, aes(x = Response, fill = question_num))+
+#   geom_bar(stat = "count", position = "stack")+
+#   facet_grid(~Q3)
+
+# q3_qol_visual_f("Q108")
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “family history” across conditions (Q26, 30, 37, 40, 46, 52, 58) more often than GCs with less years of patient-facing genetic counseling experience? 
 
@@ -117,7 +130,13 @@ q3_fh_visual_f <- function(qnum){
     facet_grid(~Q3)
 }
 
+# ggplot(q3_fh_table, aes(x = Response, fill = question_num))+
+#   geom_bar(stat = "count", position = "stack")+
+#   facet_grid(~Q3)
+# 
+# 
 # q3_fh_visual_f("Q37")
+
 
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “Variability” across conditions (Q23, 27 33, 38, 45, 56) more often than GCs with less years of patient-facing genetic counseling experience? 
@@ -134,7 +153,7 @@ q3_var_visual_f <- function(qnum){
     facet_grid(~Q3)
 }
 
-q3_var_visual_f("Q45")
+
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “reproductive autonomy” across conditions (Q29, 35, 36, 41, 47, 53, 59) more often than GCs with less years of patient-facing genetic counseling experience?
 
@@ -150,8 +169,13 @@ q3_repauto_visual_f <- function(qnum){
     facet_grid(~Q3)
 }
 
-# q3_repauto_visual_f("Q59")
 
+
+# ggplot(q3_repauto_table, aes(x = Response, fill = question_num))+
+#   geom_bar(stat = "count", position = "stack")+
+#   facet_grid(~Q3)
+# 
+# q3_repauto_visual_f("Q47")
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “Resources available” across conditions (Q25, 107, 42, 109, 54) more often than GCs with less years of patient-facing genetic counseling experience? 
 
@@ -167,7 +191,13 @@ q3_resava_visual_f <- function(qnum){
     facet_grid(~Q3)
 }
 
-q3_resava_visual_f("Q54")
+
+# ggplot(q3_resava_table, aes(x = Response, fill = question_num))+
+#   geom_bar(stat = "count", position = "stack")+
+#   facet_grid(~Q3)
+# 
+# 
+# q3_resava_visual_f("Q54")
 
 # Do the GCs tend to feel more morally uneasy with transferring affected embryos for “BRCA1 Cancer group” more often than the group “Renal Alport”?
 
