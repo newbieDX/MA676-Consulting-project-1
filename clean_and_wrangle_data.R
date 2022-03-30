69c9beba96cf14c64be6964717fd29f8b8bf3918
jyli <jinyuli@bu.edu>
2022-03-29 22:51:05 -0400

    according to our clients, combine the patient-facing experience into 4 groups and redo the visualizations.

--- clean_and_wrangle_data.R
+++ clean_and_wrangle_data.R
@@ -11,6 +11,55 @@ survey_clean <- survey_count %>% select(c(4,5,7,14,15,17:ncol(survey_count)))
 
 survey_clean_finished <- survey_clean %>% filter(Finished == "True" & Q1 == "Yes")
 
+# combine some years of patient-facing experience together
+
+survey_clean_finished <- survey_clean_finished %>% add_column(Q3_Combine = 0)
+
+for (i in 1:nrow(survey_clean_finished)) {
+  if (survey_clean_finished$Q3[i] == "Less than 1 year") {
+    survey_clean_finished$Q3_Combine[i] = "0-4 years"
+  }
+  if (survey_clean_finished$Q3[i] == "1-4 years") {
+    survey_clean_finished$Q3_Combine[i] = "0-4 years"
+  }
+  if (survey_clean_finished$Q3[i] == "5-9 years") {
+    survey_clean_finished$Q3_Combine[i] = "5-9 years"
+  }
+  if (survey_clean_finished$Q3[i] == "10-14 years") {
+    survey_clean_finished$Q3_Combine[i] = "More than 10 years"
+  }
+  if (survey_clean_finished$Q3[i] == "More than 14 years") {
+    survey_clean_finished$Q3_Combine[i] = "More than 10 years"
+  }
+}
+
+# Replace NA with 0 for Q10:
+
+survey_clean_finished$Q10[is.na(survey_clean_finished$Q10)] <- 0
+
+# combine some years of preconception experience together
+
+survey_clean_finished <- survey_clean_finished %>% add_column(Q10_Combine = 0)
+
+for (i in 1:nrow(survey_clean_finished)) {
+  if (survey_clean_finished$Q10[i] == "0") {
+    survey_clean_finished$Q10_Combine[i] = "0-3 years"
+  }
+  if (survey_clean_finished$Q10[i] == "0-3") {
+    survey_clean_finished$Q10_Combine[i] = "0-3 years"
+  }
+  if (survey_clean_finished$Q10[i] == "4-6") {
+    survey_clean_finished$Q10_Combine[i] = "4-9 years"
+  }
+  if (survey_clean_finished$Q10[i] == "7-9") {
+    survey_clean_finished$Q10_Combine[i] = "4-9 years"
+  }
+  if (survey_clean_finished$Q10[i] == "+10") {
+    survey_clean_finished$Q10_Combine[i] = "More than 10 years"
+  }
+}
+
+
 # add level to questions with response about agree level:
 
 aa <- function(x){
@@ -44,17 +93,13 @@ survey_clean_finished$Q9 <- factor(survey_clean_finished$Q9,levels = c("No", "Ye
 
 survey_clean_finished$Q7 <- factor(survey_clean_finished$Q7,levels = c("No", "Yes")) 
 
-# Replace NA with 0 for Q10:
-
-survey_clean_finished$Q10[is.na(survey_clean_finished$Q10)] <- 0
-
 # add level to Q10:
 
 survey_clean_finished$Q10 <- factor(survey_clean_finished$Q10,levels = c("0", "0-3", "4-6", "7-9","+10"))
                                             
 # Do GCs with many years of preconception experience tend to agree with Q103_1 - Q103_7 more often than GCs with less years of preconception experience? 
 
-q10_q103 <- survey_clean_finished %>% select(c(1, 4, 5, 17) | starts_with("Q103"))
+q10_q103 <- survey_clean_finished %>% select(c(1, 4, 5, 17, "Q10_Combine") | starts_with("Q103"))
 
 # Do the GCs that have encountered a patient that requested affected embryo transfer tend to agree with Q103_1 - Q103_7 more often than GCs who have not encountered a request?
 
@@ -62,31 +107,31 @@ q7_q103 <- survey_clean_finished %>% select(c(1, 4, 5, 21) | starts_with("Q103")
 
 # Do the GCs that have many years of preconception experience (Q10) tend to have encountered a patient that requested embryo transfer (Q7) more than GCs that have less years of preconception experience?
 
-q7_q10 <- survey_clean_finished %>% select(c(1, 4, 5, 21, 17))
+q7_q10 <- survey_clean_finished %>% select(c(1, 4, 5, 21, 17, "Q10_Combine"))
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with Q103_1 - Q103_7 more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_q103 <- survey_clean_finished %>% select(c(1, 4, 5, 12) | starts_with("Q103"))
+q3_q103 <- survey_clean_finished %>% select(c(1, 4, 5, 12, "Q3_Combine") | starts_with("Q103"))
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “quality of life” across conditions (Q24, Q31, 32, 108, 113, 51, 110) more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_qol <- survey_clean_finished %>% select(c(1, 4, 5, 12, 32, 37, 41, 46, 52, 57, 62))
+q3_qol <- survey_clean_finished %>% select(c(1, 4, 5, 12, 32, 37, 41, 46, 52, 57, 62, "Q3_Combine"))
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “family history” across conditions (Q26, 30, 37, 40, 46, 52, 58) more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_fh <- survey_clean_finished %>% select(c(1, 4, 5, 12, 34, 38, 43, 48, 54, 58, 63))
+q3_fh <- survey_clean_finished %>% select(c(1, 4, 5, 12, 34, 38, 43, 48, 54, 58, 63, "Q3_Combine"))
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “Variability” across conditions (Q23, 27 33, 38, 45, 56) more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_var <- survey_clean_finished %>% select(c(1, 4, 5, 12, 31, 36, 40, 45, 51, 61))
+q3_var <- survey_clean_finished %>% select(c(1, 4, 5, 12, 31, 36, 40, 45, 51, 61, "Q3_Combine"))
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “reproductive autonomy” across conditions (Q29, 35, 36, 41, 47, 53, 59) more often than GCs with less years of patient-facing genetic counseling experience?
 
-q3_repauto <- survey_clean_finished %>% select(c(1, 4, 5, 12, 35, 39, 44, 50, 56, 60, 64))
+q3_repauto <- survey_clean_finished %>% select(c(1, 4, 5, 12, 35, 39, 44, 50, 56, 60, 64, "Q3_Combine"))
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “Resources available” across conditions (Q25, 107, 42, 109, 54) more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_resava <- survey_clean_finished %>% select(c(1, 4, 5, 12, 33, 42, 47, 53, 59))
+q3_resava <- survey_clean_finished %>% select(c(1, 4, 5, 12, 33, 42, 47, 53, 59, "Q3_Combine"))
 
 # Do the GCs tend to feel more morally uneasy with transferring affected embryos  for “BRCA1 Cancer group” more often than the group “Renal Alport”?
 
--- vfunctions.R
+++ vfunctions.R
@@ -3,18 +3,28 @@ library("ggplot2")
 
 # Do GCs with many years of preconception experience tend to agree with Q103_1 - Q103_7 more often than GCs with less years of preconception experience? 
 
-q10_q103_visual <- q10_q103 %>% select(c(1,4:11))
+q10_q103_visual <- q10_q103 %>% select(c(1,4:ncol(q10_q103)))
 q10_q103_table <- q10_q103_visual %>%
-  pivot_longer(-c(IPAddress,Q10), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q10,Q10_Combine), names_to = "question_num", values_to = "Response")
+
+
+
 
 q10_q103_visual_f <- function(qnum){
   df <- q10_q103_table %>% filter(question_num == qnum)
   
-  ggplot(df, aes(x = Response, fill = Q10))+
+  ggplot(df, aes(x = Response, fill = Q10_Combine))+ # or fill = Q10
     geom_bar(stat = "count", position = "stack")+
-    facet_grid(~Q10)
+    facet_grid(~Q10_Combine)
 }
 
+q10_q103_visual_f("Q103_1")
+q10_q103_visual_f("Q103_2")
+q10_q103_visual_f("Q103_3")
+q10_q103_visual_f("Q103_4")
+q10_q103_visual_f("Q103_5")
+q10_q103_visual_f("Q103_6")
+q10_q103_visual_f("Q103_7")
 
 # Relatively high corr qnum: q10 with q103_2, q103_3, q103_4
 
@@ -59,13 +69,15 @@ q7_q103_visual_f <- function(qnum){
 
 # Do the GCs that have many years of preconception experience (Q10) tend to have encountered a patient that requested embryo transfer (Q7) more than GCs that have less years of preconception experience?
 
-q7_q10_visual <- q7_q10 %>% select(c(1,4,5))
+q7_q10_visual <- q7_q10 %>% select(c(1,4:ncol(q7_q10)))
 q7_q10_table <- q7_q10_visual %>%
-  pivot_longer(-c(IPAddress,Q10), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q10,Q10_Combine), names_to = "question_num", values_to = "Response")
 
 q7_q10_visual_plot <- ggplot(q7_q10_table, aes(x = Response, fill = Response))+
   geom_bar(stat = "count", position = "stack")+
-  facet_grid(~Q10)
+  facet_grid(~Q10_Combine)
+
+q7_q10_visual_plot
 
 # Positive corrlation between Q7 and Q10
 
@@ -73,17 +85,27 @@ q7_q10_visual_plot <- ggplot(q7_q10_table, aes(x = Response, fill = Response))+
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with Q103_1 - Q103_7 more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_q103_visual <- q3_q103 %>% select(c(1,4:11))
+q3_q103_visual <- q3_q103 %>% select(c(1,4:ncol(q3_q103)))
 q3_q103_table <- q3_q103_visual %>%
-  pivot_longer(-c(IPAddress,Q3), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q3,Q3_Combine), names_to = "question_num", values_to = "Response")
 
 q3_q103_visual_f <- function(qnum){
   df <- q3_q103_table %>% filter(question_num == qnum)
   
-  ggplot(df, aes(x = Response, fill = Q3))+
+  ggplot(df, aes(x = Response, fill = Q3_Combine))+
     geom_bar(stat = "count", position = "stack")+
-    facet_grid(~Q3)
+    facet_grid(~Q3_Combine)
 }
+
+q3_q103_visual_f("Q103_1")
+q3_q103_visual_f("Q103_2")
+q3_q103_visual_f("Q103_3")
+q3_q103_visual_f("Q103_4")
+q3_q103_visual_f("Q103_5")
+q3_q103_visual_f("Q103_6")
+q3_q103_visual_f("Q103_7")
+
+
 
 # ggplot(q3_q103_table, aes(x = Response, fill = question_num))+
 #   geom_bar(stat = "count", position = "stack")+
@@ -97,18 +119,25 @@ q3_q103_visual_f <- function(qnum){
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “quality of life” across conditions (Q24, Q31, 32, 108, 113, 51, 110) more often than GCs with less years of patient-facing genetic counseling experience? 
 
 
-q3_qol_visual <- q3_qol %>% select(c(1,4:11))
+q3_qol_visual <- q3_qol %>% select(c(1,4:ncol(q3_qol)))
 q3_qol_table <- q3_qol_visual %>%
-  pivot_longer(-c(IPAddress,Q3), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q3,Q3_Combine), names_to = "question_num", values_to = "Response")
 
 q3_qol_visual_f <- function(qnum){
   df <- q3_qol_table %>% filter(question_num == qnum)
   
-  ggplot(df, aes(x = Response, fill = Q3))+
+  ggplot(df, aes(x = Response, fill = Q3_Combine))+
     geom_bar(stat = "count", position = "stack")+
-    facet_grid(~Q3)
+    facet_grid(~Q3_Combine)
 }
-
+#Q24, Q31, 32, 108, 113, 51, 110
+q3_qol_visual_f("Q24")
+q3_qol_visual_f("Q31")
+q3_qol_visual_f("Q32")
+q3_qol_visual_f("Q108")
+q3_qol_visual_f("Q113")
+q3_qol_visual_f("Q51")
+q3_qol_visual_f("Q110")
 
 # ggplot(q3_qol_table, aes(x = Response, fill = question_num))+
 #   geom_bar(stat = "count", position = "stack")+
@@ -118,17 +147,26 @@ q3_qol_visual_f <- function(qnum){
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “family history” across conditions (Q26, 30, 37, 40, 46, 52, 58) more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_fh_visual <- q3_fh %>% select(c(1,4:11))
+q3_fh_visual <- q3_fh %>% select(c(1,4:ncol(q3_fh)))
 q3_fh_table <- q3_fh_visual %>%
-  pivot_longer(-c(IPAddress,Q3), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q3,Q3_Combine), names_to = "question_num", values_to = "Response")
 
 q3_fh_visual_f <- function(qnum){
   df <- q3_fh_table %>% filter(question_num == qnum)
   
-  ggplot(df, aes(x = Response, fill = Q3))+
+  ggplot(df, aes(x = Response, fill = Q3_Combine))+
     geom_bar(stat = "count", position = "stack")+
-    facet_grid(~Q3)
+    facet_grid(~Q3_Combine)
 }
+# Q26, 30, 37, 40, 46, 52, 58
+q3_fh_visual_f("Q26")
+q3_fh_visual_f("Q30")
+q3_fh_visual_f("Q37")
+q3_fh_visual_f("Q40")
+q3_fh_visual_f("Q46")
+q3_fh_visual_f("Q52")
+q3_fh_visual_f("Q58")
+
 
 # ggplot(q3_fh_table, aes(x = Response, fill = question_num))+
 #   geom_bar(stat = "count", position = "stack")+
@@ -141,34 +179,47 @@ q3_fh_visual_f <- function(qnum){
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “Variability” across conditions (Q23, 27 33, 38, 45, 56) more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_var_visual <- q3_var %>% select(c(1,4:10))
+q3_var_visual <- q3_var %>% select(c(1,4:ncol(q3_var)))
 q3_var_table <- q3_var_visual %>%
-  pivot_longer(-c(IPAddress,Q3), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q3,Q3_Combine), names_to = "question_num", values_to = "Response")
 
 q3_var_visual_f <- function(qnum){
   df <- q3_var_table %>% filter(question_num == qnum)
   
-  ggplot(df, aes(x = Response, fill = Q3))+
+  ggplot(df, aes(x = Response, fill = Q3_Combine))+
     geom_bar(stat = "count", position = "stack")+
-    facet_grid(~Q3)
+    facet_grid(~Q3_Combine)
 }
 
-
+# Q23, 27, 33, 38, 45, 56
+q3_var_visual_f("Q23")
+q3_var_visual_f("Q27")
+q3_var_visual_f("Q33")
+q3_var_visual_f("Q38")
+q3_var_visual_f("Q45")
+q3_var_visual_f("Q56")
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “reproductive autonomy” across conditions (Q29, 35, 36, 41, 47, 53, 59) more often than GCs with less years of patient-facing genetic counseling experience?
 
-q3_repauto_visual <- q3_repauto %>% select(c(1,4:11))
+q3_repauto_visual <- q3_repauto %>% select(c(1,4:ncol(q3_repauto)))
 q3_repauto_table <- q3_repauto_visual %>%
-  pivot_longer(-c(IPAddress,Q3), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q3,Q3_Combine), names_to = "question_num", values_to = "Response")
 
 q3_repauto_visual_f <- function(qnum){
   df <- q3_repauto_table %>% filter(question_num == qnum)
   
-  ggplot(df, aes(x = Response, fill = Q3))+
+  ggplot(df, aes(x = Response, fill = Q3_Combine))+
     geom_bar(stat = "count", position = "stack")+
-    facet_grid(~Q3)
+    facet_grid(~Q3_Combine)
 }
-
+# Q29, 35, 36, 41, 47, 53, 59
+q3_repauto_visual_f("Q29")
+q3_repauto_visual_f("Q35")
+q3_repauto_visual_f("Q36")
+q3_repauto_visual_f("Q41")
+q3_repauto_visual_f("Q47")
+q3_repauto_visual_f("Q53")
+q3_repauto_visual_f("Q59")
 
 
 # ggplot(q3_repauto_table, aes(x = Response, fill = question_num))+
@@ -179,18 +230,23 @@ q3_repauto_visual_f <- function(qnum){
 
 # Do GCs with many years of patient-facing genetic counseling experience (Q3) tend to agree with the embryo transfer justification of “Resources available” across conditions (Q25, 107, 42, 109, 54) more often than GCs with less years of patient-facing genetic counseling experience? 
 
-q3_resava_visual <- q3_resava %>% select(c(1,4:9))
+q3_resava_visual <- q3_resava %>% select(c(1,4:ncol(q3_resava)))
 q3_resava_table <- q3_resava_visual %>%
-  pivot_longer(-c(IPAddress,Q3), names_to = "question_num", values_to = "Response")
+  pivot_longer(-c(IPAddress,Q3,Q3_Combine), names_to = "question_num", values_to = "Response")
 
 q3_resava_visual_f <- function(qnum){
   df <- q3_resava_table %>% filter(question_num == qnum)
   
-  ggplot(df, aes(x = Response, fill = Q3))+
+  ggplot(df, aes(x = Response, fill = Q3_Combine))+
     geom_bar(stat = "count", position = "stack")+
-    facet_grid(~Q3)
+    facet_grid(~Q3_Combine)
 }
-
+# Q25, 107, 42, 109, 54
+q3_resava_visual_f("Q25")
+q3_resava_visual_f("Q107")
+q3_resava_visual_f("Q42")
+q3_resava_visual_f("Q109")
+q3_resava_visual_f("Q54")
 
 # ggplot(q3_resava_table, aes(x = Response, fill = question_num))+
 #   geom_bar(stat = "count", position = "stack")+
